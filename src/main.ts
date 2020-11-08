3bacf8e5d7651664cf346292e9a4cdbeee3541b1
Aleksander Sorokin <ankezy@gmail.com>
2020-11-08 13:54:46 +0300

    /: enable cors for allowed origins

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const serverConfig = config.get<{ port: number }>('server');
+  const serverConfig = config.get<{
+    port: number;
+    origin: CorsOptions['origin'];
+  }>('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  const origin = serverConfig.origin;
   const port = process.env.PORT || serverConfig.port;
+
+  app.enableCors({ origin });
+  logger.log(`Accepting requests from origin "${origin}"`);
+
   await app.listen(port);
-  logger.log(`Application listening on port ${port}`);
+  logger.log(`Application listening on port "${port}"`);
 }
 bootstrap();
